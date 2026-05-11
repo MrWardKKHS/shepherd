fd4efffdfb89842876ace6ce7f03719970aafe05
Alex Ward <pianowithalex@gmail.com>
2026-05-11 20:51:43 +1200

    marked up and styled index

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from livereload import Server
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Hot reload using live server
+    server = Server(app.wsgi_app)
+    server.watch("templates/")
+    server.watch("static/")
+    server.watch("static/")
+    server.serve(
+        host="0.0.0.0",
+        port=5000,
+        liveport=35729,
+        debug=True
+    )
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Resources:
+    Sticky nav: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+    Background: https://unsplash.com/@scotty_from_acorn
